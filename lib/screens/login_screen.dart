de4d38c22a474a60a1a13def88cc9a3f3075df46
Fabio Hideki <hideki.fabio@gmail.com>
2020-09-18 13:59:16 -0300

    adding validadors to login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/helpers/validators.dart';
 
 class LoginScreen extends StatelessWidget {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,51 +16,64 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            padding: const EdgeInsets.all(16),
-            shrinkWrap: true,
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                validator: (email) {
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Senha'),
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                obscureText: true,
-                validator: (pass) {
-                  if (pass.isEmpty || pass.length < 6) {
-                    return 'Senha inválida';
-                  }
-                  return null;
-                },
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: FlatButton(
-                    onPressed: () {},
-                    padding: EdgeInsets.zero,
-                    child: const Text('Esqueci minha senha')),
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                height: 44,
-                child: RaisedButton(
-                  onPressed: () {},
-                  color: Theme.of(context).primaryColor,
-                  child: const Text(
-                    'Entrar',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: <Widget>[
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(hintText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  validator: (email) {
+                    if (!emailValid(email)) {
+                      return 'E-mail invalid';
+                    }
+                    return null;
+                  },
                 ),
-              )
-            ],
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: passController,
+                  decoration: const InputDecoration(hintText: 'Password'),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  obscureText: true,
+                  validator: (pass) {
+                    if (pass.isEmpty || pass.length < 6) {
+                      return 'Password invalid';
+                    }
+                    return null;
+                  },
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                      onPressed: () {},
+                      padding: EdgeInsets.zero,
+                      child: const Text('Forgot password')),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (formKey.currentState.validate()) {
+                        print(emailController.text);
+                        print(passController.text);
+                      }
+                    },
+                    color: Theme.of(context).primaryColor,
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
